f0a836e17b148488484170f7657b5645530043a5
Murray Spork <murray.spork@gmail.com>
2013-04-24 15:26:28 -0700

    fix config for heroku mongo db

--- server/config/init.rb
+++ server/config/init.rb
@@ -12,25 +12,21 @@ class NotesApp < Sinatra::Base
 	  # require 'sqlite3'
 	  set :views, File.dirname(__FILE__) + '/../views'	  
 	end
+	
 	configure :test do
 		DB = Mongo::MongoClient.new("localhost", 27017).db('learning-mongo')	
 	  # set :database, 'sqlite::memory:'
 	  # rspec context looks for views relative to /spec dir unless specified
 	  set :views, File.dirname(__FILE__) + '/../views'
 	end
+
 	configure :production do
 	  # Sequel.connect(ENV['DATABASE_URL'])
 	  set :views, File.dirname(__FILE__) + '/../views'
-	  DB = get_connection
-	end
-
-
-	def self.get_connection
 	  db = URI.parse(ENV['MONGOHQ_URL'])
 	  db_name = db.path.gsub(/^\//, '')
-	  db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-	  db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-	  db_connection
+	  DB = Mongo::Connection.new(db.host, db.port).db(db_name)
+	  DB.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)	
 	end
 
 end
