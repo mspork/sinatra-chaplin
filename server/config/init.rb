996b738c9615ad1c1f0ace11139b31ea65cdb59c
Murray Spork <murray.spork@gmail.com>
2013-04-24 15:14:23 -0700

    config for heroku mongo db

--- server/config/init.rb
+++ server/config/init.rb
@@ -1,16 +1,19 @@
 require 'mongo'
- 
+require 'uri'
+
+
 
 class NotesApp < Sinatra::Base
-	DB = Mongo::MongoClient.new("localhost", 27017).db('learning-mongo')
 	# DB = Mongo::Connection.new.db("learning-mongo")
 
 	configure :development do
+		DB = Mongo::MongoClient.new("localhost", 27017).db('learning-mongo')
 	  # set :database, 'sqlite://development/my_app.db'
 	  # require 'sqlite3'
 	  set :views, File.dirname(__FILE__) + '/../views'	  
 	end
 	configure :test do
+		DB = Mongo::MongoClient.new("localhost", 27017).db('learning-mongo')	
 	  # set :database, 'sqlite::memory:'
 	  # rspec context looks for views relative to /spec dir unless specified
 	  set :views, File.dirname(__FILE__) + '/../views'
@@ -18,7 +21,19 @@ class NotesApp < Sinatra::Base
 	configure :production do
 	  # Sequel.connect(ENV['DATABASE_URL'])
 	  set :views, File.dirname(__FILE__) + '/../views'
+	  DB = get_connection
 	end
+
+
+	def get_connection
+	  return @db_connection if @db_connection
+	  db = URI.parse(ENV['MONGOHQ_URL'])
+	  db_name = db.path.gsub(/^\//, '')
+	  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+	  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+	  @db_connection
+	end
+
 end
  
 require_relative 'migrations'
