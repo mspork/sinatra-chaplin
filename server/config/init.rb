89e132e4657feacdc4ce7b0b4e69bd2f89ee4f9c
Murray Spork <murray.spork@gmail.com>
2013-04-24 15:19:55 -0700

    fix config for heroku mongo db

--- server/config/init.rb
+++ server/config/init.rb
@@ -25,13 +25,12 @@ class NotesApp < Sinatra::Base
 	end
 
 
-	def get_connection
-	  return @db_connection if @db_connection
+	def self.get_connection
 	  db = URI.parse(ENV['MONGOHQ_URL'])
 	  db_name = db.path.gsub(/^\//, '')
-	  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-	  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-	  @db_connection
+	  db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+	  db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+	  db_connection
 	end
 
 end
