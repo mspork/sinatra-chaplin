8797aebc30663544f7e687fe016ecd0c887cd107
Murray Spork <murray.spork@gmail.com>
2013-05-07 23:30:31 -0700

    editing notes is now working

--- server/public/js/mobile/application.js
+++ server/public/js/mobile/application.js
@@ -4,22 +4,40 @@ define(
 	'handlebars',
 	'zepto',
 	'foundation',
-	'foundationtopbar',
 	'text!templates/mobile/profile.hbs',
 	'text!templates/mobile/notes.hbs',
-	'text!templates/mobile/note.hbs'
+	'text!templates/mobile/note.hbs',
+	'text!templates/mobile/noteEdit.hbs',
+	'foundationtopbar'
 	],
 	function(
 		Backbone, Handlebars,
-		Zepto, Foundation, FoundationTopbar,
-		profileTemplate, notesTemplate, noteTemplate) {
+		Zepto, Foundation,
+		profileTemplate, notesTemplate, noteTemplate, noteEditTemplate) {
+
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+      if (o[this.name] !== undefined) {
+          if (!o[this.name].push) {
+              o[this.name] = [o[this.name]];
+          }
+          o[this.name].push(this.value || '');
+      } else {
+          o[this.name] = this.value || '';
+      }
+  });
+  return o;
+};
+
 
 		var Notes = Backbone.Collection.extend({
 			url: '/api/notes'
 		});
 
 		var Note = Backbone.Model.extend({
-			urlRoot: '/api/note'
+			urlRoot: '/api/notes'
 		});
 
 		var NoteListView = Backbone.View.extend({
@@ -43,8 +61,49 @@ define(
 				this.model.on('show', this.render, this);
 			},
 			render: function(){
+				console.log('NoteView#render');
 				var json = this.model.toJSON();
 				var templateFunc = Handlebars.compile(noteTemplate);
+				this.$el.html(templateFunc( json ));
+			}
+		});
+
+		var NoteEditView = Backbone.View.extend({
+			el: '#content-container',
+
+			events: {
+				'click #save': 'saveUser'
+			},
+
+			initialize: function(){
+				this.model.on('edit', this.render, this);
+			},
+
+			saveUser: function(ev){
+				var noteDetails = $('form#note').serializeObject();
+				// noteDetails['id'] = this.model.id;
+				var note = new Note( {'id': this.model.id} );
+				note.save(noteDetails, {
+					success: function(note, response){
+						console.log('save successful');
+						var route = "notes/" + note.id;
+						// TODO: is there anyway we can get rid of using window global
+						// For e.g. something like this? (tried it and did not work)
+						// note.trigger('show');						
+						window.app.router.navigate(route, {trigger: true});
+					},
+					error: function(model, xhr, options){
+						console.log('error on save');
+						console.log(xhr);
+					}
+				});
+				return false;
+			},
+
+			render: function(){
+				console.log('NoteEditView#render');
+				var json = this.model.toJSON();
+				var templateFunc = Handlebars.compile(noteEditTemplate);
 				this.$el.html(templateFunc( json ));
 			}
 		});
@@ -64,6 +123,7 @@ define(
 			routes: {
 				'': 'notes',
 				'notes': 'notes',
+				'notes/:id/edit': 'editNote',
 				'notes/:id': 'openNote',
 				'profile': 'profile'
 			}
@@ -100,12 +160,33 @@ define(
 				noteView = new NoteView( {model: note });
 				note.trigger('show');
 			} else {
-				self.collections.notes.fetch({
+				note = new Note( {id: id});
+				note.fetch({
 					success: function() {
-							note = self.collections.notes.get(id);
 							console.log(note);
 							noteView = new NoteView( {model: note });
 							note.trigger('show');
+					}
+				});
+			}
+		});
+
+		this.router.on('route:editNote', function(id) {
+			console.log("edit id: " + id);
+			var note, noteEditView;
+			// TODO: clean this up - factor out commonalities
+			if(self.collections.length > 0){
+				note = self.collections.notes.get(id);
+				console.log('Note already in collection: ' + note);
+				noteEditView = new NoteEditView( {model: note });
+				note.trigger('edit');
+			} else {
+				note = new Note( {id: id});
+				note.fetch({
+					success: function() {
+							console.log('Note had to be retrieved individually' + note);
+							noteEditView = new NoteEditView( {model: note });
+							note.trigger('edit');
 					}
 				});
 			}
